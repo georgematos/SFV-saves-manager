0d0e8e96f81713456237fd7234e48cb7825c7b7d
George Matos <georgemattos@gmail.com>
2020-08-21 14:32:47 -0300

    inclusao do firebase no app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from '../environments/environment';
+import * as firebase from 'firebase/app';
+import "firebase/auth";
 
 @Component({
   selector: 'app-root',
@@ -24,5 +26,18 @@ export class AppComponent {
     } else {
       console.log('Run in browser');
     }
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDz0EMfsy1XmZwNQa2xg4rEdL7AuiH6yQk",
+      authDomain: "sfv-saves-manager.firebaseapp.com",
+      databaseURL: "https://sfv-saves-manager.firebaseio.com",
+      projectId: "sfv-saves-manager",
+      storageBucket: "sfv-saves-manager.appspot.com",
+      messagingSenderId: "923011523319",
+      appId: "1:923011523319:web:3e022326bc57a73e9c29f2",
+      measurementId: "G-JWD2JW6WJR"
+    };
+
+    firebase.initializeApp(firebaseConfig);
   }
 }
