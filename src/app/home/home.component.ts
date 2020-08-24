a66e88d7f0bc3585b2533eefc131b5cff94dfc7b
George Matos <georgemattos@gmail.com>
2020-08-24 13:41:06 -0300

    conclusao de layout da tela home, inclusao de estilo para botões redondos e inclusao de modal para inclusao de conta steam

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FirebaseService } from 'app/core/services/firebase/firebase.service';
 import * as firebase from 'firebase/app';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +12,12 @@ import * as firebase from 'firebase/app';
 export class HomeComponent implements OnInit {
 
   public currentUserEmail: string;
+  public modalForm: FormGroup;
 
   constructor(
     private router: Router,
-    private firebaseService: FirebaseService
+    private firebaseService: FirebaseService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -25,6 +28,17 @@ export class HomeComponent implements OnInit {
         console.log('User is not logged');
       }
     })
+    this.modalForm = this.formBuilder.group({
+      conta: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+      ])]
+    })
+  }
+
+  public save(): void {
+    console.log(this.modalForm.value.conta);
   }
 
   public logout(): void {
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './home.component';
-import { HomeRoutingModule } from './home-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthGuard } from 'app/core/services/firebase/auth-guard-service';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
+    CommonModule,
+    SharedModule,
     HomeRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
-    AuthGuard
+    AuthGuard,
   ]
 })
 export class HomeModule { }
